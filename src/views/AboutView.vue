1beff999c69d1b5b46159dd4576f5ed1f4c27436
Hermann Caraballo <hermann.caraballo@gmail.com>
2023-05-08 08:55:43 -0400

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="about mt-60">
+    <!-- <h1>This is an about page</h1> -->
+    <p>I've been in the IT business for over 13 years as a Web Developer usually for
+advertising agencies or consulting companies and for the last 6 years I've
+been specialized on frontend development, however I have fullstack basis, I
+also have experience on Tech Lead and Project Management.</p>
+<p>As the greatest achievements of mi professional career I consider reaching a
+level where according to my skills and knowledge I can be required to do the
+job and even be taken as an example, this of course, is reflected on my
+developments, some about websites with a high traffic or maybe for abroad
+and when you notice that (as a final user) it happens and it's working just fine,
+it's priceless.</p>
+<p>I consider myself a dedicated person, I am constantly learning, checking
+news, reading about newest stuff, tech, processes, better dev practices, etc.
+and this, personally, has really impacted on me for academics and for the
+professional field.</p>
+<p>I studied at the Universidad Central de Venezuela, Licienciatura en Ciencias
+de la Computación (Computer Science); completed and approved IBM
+Advanced Career Education for e-business, besides a series of courses,
+workshops and online tutorials (mainly from Udemy and CodeSchools) which I
+am constantly checking to keep up, with this I have managed to get the
+experience I currently own.</p>
+<p>I've been working for over the last two years for companies abroad, however, I
+saw this job offer and the benefits are really interesting. I have specialized in
+Frontend development for the last 7 years, programming on CSS
+preprocessors like Saas and layouts are one of the most enjoyable tasks for
+me, I have a vast experience working with JavaScript based Frameworks like
+Angular, Vue and React so I think I'd be a valuable asset for this company. Of
+course I am also bilingual and have experience on both cascade and agile
+methodologies.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/LabView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="lab">
+        <!-- <h1>This is a lab page</h1> -->
+        <div class="w-11/12 mx-auto rounded-xl shadow-lg mb-20 mt-20">
+            <router-link to="/pokeAPI">
+                <h1 class="font-black md:text-3xl text-xt green mb-2">Pokemon API</h1>
+                <img src="../assets/img/poke_API.png" alt="thumbnail of project">
+            </router-link>
+        </div>
+        <div class="w-11/12 mx-auto rounded-xl shadow-lg mb-20">
+            <router-link to="todoLIST">
+                <h1 class="font-black md:text-3xl text-xt green mb-2">ToDo List</h1>
+                <img src="../assets/img/todo_list.png" alt="thumbnail of project">
+            </router-link>
+        </div>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .lab {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+  </style>
